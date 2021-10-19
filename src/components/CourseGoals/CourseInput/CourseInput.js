a8923b21175e6f894c842c541a7c2bc61c8c2d78
rrajput1994 <92074737+rrajput1994@users.noreply.github.com>
2021-10-19 20:38:42 +0530

    dynamic styles and media queries using CSS-Modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,44 +1,44 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${(props) => (props.invalid ? "red" : "")};
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props) => (props.invalid ? "red" : "")};
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${(props) => (props.invalid ? "red" : "")};
-    background: ${(props) => (props.invalid ? "rgb(228, 154, 154)" : "")};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.invalid ? "red" : "")};
+//     background: ${(props) => (props.invalid ? "rgb(228, 154, 154)" : "")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
 
-  // &.invalid label {
-  //   color: red;
-  // }
+//   &.invalid label {
+//     color: red;
+//   }
 
-  // &.invalid input {
-  //   border-color: rgb(175, 33, 33);
-  //   background-color: rgb(228, 154, 154);
-  // }
-`;
+//   &.invalid input {
+//     border-color: rgb(175, 33, 33);
+//     background-color: rgb(228, 154, 154);
+//   }
+// `;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -66,14 +66,14 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      {/* <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input
           type="text"
           onChange={goalInputChangeHandler}
           value={enteredValue}
         />
-      </FormControl>
+      </FormControl> */}
 
       {/* <FormControl className={!isValid ? "invalid" : ""}>
         <label>Course Goal</label>
@@ -84,14 +84,16 @@ const CourseInput = (props) => {
         />
       </FormControl> */}
 
-      {/* <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input
           type="text"
           onChange={goalInputChangeHandler}
           value={enteredValue}
         />
-      </div> */}
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
